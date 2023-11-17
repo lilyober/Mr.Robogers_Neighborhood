09d23b1205cbd0edf2999fafc5432ee9e8b65328
lilyob <lilyober07@gmail.com>
2023-11-17 13:13:21 -0800

    WIP first function.

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+// UI Logic
+
+// function result(){
+//     for (inputed num < i++ {
+//         if sum = sum + 1 === functionOne{
+//             then "Bleep"
+//         }
+//     })
+
+
+//  results
+// }
+
+
+
+
+
+
+
+// Business Logic
+
+function functionOne(){
+
+};
+
+function functionTwo(){
+    
+};
+
+function functionThree(){
+    
+};
+
+function sentenceGen( ){
+    // calls on result function
+}
