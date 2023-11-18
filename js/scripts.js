3fb9ebfe41491ffe79f646cba2d29d99cba3dc51
lilyob <lilyober07@gmail.com>
2023-11-17 17:39:21 -0800

    WIP making an array.

--- js/scripts.js
+++ js/scripts.js
@@ -18,22 +18,19 @@
 
 
 // Business Logic
-window.onload = function () {
-    let form = document.getElementById("putInANum");
-    form.addEventListener("submit", (event) => {
-        event.preventDefault();
-        let inputNum = document.querySelector("number").value;
-        let array = [];
+// window.onload = function () {
+//     let form = document.getElementById("putInANum");
+//     form.addEventListener("submit", (event) => {
+//         event.preventDefault();
+let inputNum = document.getElementById("number").value;
+let array = [];
 
-        for (let index = 0; index <= inputNum; index + 1) {
-            array.push(index);
-        };
-
-        let p = document.getElementById("results");
-        p.innerText = array;
-    });
-
+for (let index = 0; index <= inputNum; index + 1) {
+    array.push(array[index] + 1);
 };
+
+let p = document.getElementById("results");
+p.innerText = array;
 
 
 
