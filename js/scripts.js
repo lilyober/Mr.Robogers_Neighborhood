da516d7a3865eed70236750af4b3f4d0cd0bc3c9
lilyob <lilyober07@gmail.com>
2023-11-17 19:18:53 -0800

    Add for loop that adds numbers to array.

--- js/scripts.js
+++ js/scripts.js
@@ -19,42 +19,22 @@
 
 // Business Logic
 
-
 function makingAr() {
-    let array = [];
     const inputNum = document.getElementById("number").value;
-    array.push(inputNum);
-    // let p = document.getElementById("results");
-    // p.innerText = array;
+    let array = [];
+
+    for (let index = 0; index <= inputNum; index += 1) {
+        array.push(index);
+    };
+
+    let p = document.getElementById("results");
+    p.innerText = array.join(", ");
 };
-
-function addingNewAr() {
-    makingAr();
-    let newArray = [];
-    for (let index = 0; index <= array; index += 1) {
-        newArray.push(index);
-        console.log(index);
-    }
-};
-
-//     for (let index = 0; index <= inputNum; index + 1) {
-//         array.push(array[index] + 1);
-//     };
-
-//     let p = document.getElementById("results");
-//     p.innerText = array;
-// }
 
 window.onload = function () {
     let form = document.getElementById("putInANum");
     form.addEventListener("submit", (event) => {
         event.preventDefault();
-        addingNewAr();
+        makingAr();
     });
 };
-
-
-
-// function sentenceGen( ){
-//     calls on result function
-// }
