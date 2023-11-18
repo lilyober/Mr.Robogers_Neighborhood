f3fc5bfb2297035f1931b417b973b2a3f0660c18
lilyob <lilyober07@gmail.com>
2023-11-17 18:44:00 -0800

    Add an array for user input.

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ Test: "Make an array from the user's inputed number"
 Code: 3;
 Expected Output: [3];
 
-Test: "Add numbers to the array beginning from 0 until you hit the user's inputted number."
-Code: 3;
-Expected Output: [1, 2, 3];
+Describe: 
 
+Test: "Add the numbers leading up to the inputted number into the array"
+Code: 3;
+Expected Output: [0, 1, 2, 3];
--- js/scripts.js
+++ js/scripts.js
@@ -21,22 +21,35 @@
 
 
 function makingAr() {
-    let inputNum = document.getElementById("number").value;
     let array = [];
+    const inputNum = document.getElementById("number").value;
+    array.push(inputNum);
+    // let p = document.getElementById("results");
+    // p.innerText = array;
+};
 
-    for (let index = 0; index <= inputNum; index + 1) {
-        array.push(array[index] + 1);
-    };
+function addingNewAr() {
+    makingAr();
+    let newArray = [];
+    for (let index = 0; index <= array; index += 1) {
+        newArray.push(index);
+        console.log(index);
+    }
+};
 
-    let p = document.getElementById("results");
-    p.innerText = array;
-}
+//     for (let index = 0; index <= inputNum; index + 1) {
+//         array.push(array[index] + 1);
+//     };
+
+//     let p = document.getElementById("results");
+//     p.innerText = array;
+// }
 
 window.onload = function () {
     let form = document.getElementById("putInANum");
     form.addEventListener("submit", (event) => {
         event.preventDefault();
-        makingAr();
+        addingNewAr();
     });
 };
 
