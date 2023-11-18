8cc0a183ecf019d325576a860bfb22d931cc9251
lilyob <lilyober07@gmail.com>
2023-11-18 13:24:18 -0800

    WIP making a function.

--- README.md
+++ README.md
@@ -10,7 +10,14 @@ Expected Output: [1, 2, 3];
 
 Describe: results();
 
-Test: "When one is present, change it to "Bleep!" When two is present, change it to "Bloop!" When three is present, change it to "Won't you be my neighbor?"
+Test: "When one is present, change it to "Bleep!"
 Code: 3;
-Expected Output: [1, 2, 3];
+Expected Output: [Bleep!, 2, 3];
 
+Test: "When two is present, change it to "Bloop!"
+Code: 3;
+Expected Output: [Bleep!, Boop!, 3];
+
+Test: "When three is present, change it to "Won't you be my neighbor!"
+Code: 3;
+Expected Output: [Bleep!, Boop!, Won't you be my neighbor?];
--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,42 @@
 // UI Logic
-// function results() {
-//     let resultAr = [];
-//     let array = [];
-//     makingAr();
-//     array.forEach(function (element) {
-//         if (element.includes(1)) {
-//             resultAr.push("Bleep!");
-//         } else {
-//             resultAr.push(element);
-//         };
-//         console.log(resultAr);
-//         let p = document.getElementById("results");
-//         p.innerText = resultAr.join(", ");
-//     });
-// };
+function results() {
+    let resultAr = [];
+
+    arrayInput.forEach(function (element) {
+        if (element.includes(1)) {
+            resultAr.push("Bleep!");
+        } else if (array.includes(2)) {
+            resultAr.push("Bloop!");
+        } else if (array.includes(3)) {
+            resultAr.push("Won't you be my neighbor?");
+        } else {
+            resultAr.push(element);
+        }
+    });
+    // let p = document.getElementById("results");
+    // p.innerText = resultAr.join(", ");
+};
 
 // Business Logic
+let arrayInput = [];
 function makingAr() {
-    let array = [];
+    let arrayInput = [];
     const inputNum = document.getElementById("number").value;
 
     for (let index = 0; index <= inputNum; index += 1) {
-        array.push(index);
+        arrayInput.push(index);
     };
-};
 
-function results() {
-    let resultAr = [];
-    let array = [];
-    makingAr();
-    array.forEach(function (element) {
-        if (element.includes(1)) {
-            resultAr.push("Bleep!");
-        } else {
-            resultAr.push(element);
-        };
-        console.log(resultAr);
-        let p = document.getElementById("results");
-        p.innerText = resultAr.join(", ");
-    });
+    results(arrayInput);
+
+    let p = document.getElementById("results");
+    p.innerText = resultAr.join(", ");
 };
 
 window.onload = function () {
     let form = document.getElementById("putInANum");
     form.addEventListener("submit", (event) => {
         event.preventDefault();
-        results();
+        makingAr();
     });
 };
