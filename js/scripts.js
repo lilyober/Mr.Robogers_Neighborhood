2779acabcfe31595896f733b59b350b703b1169a
lilyob <lilyober07@gmail.com>
2023-11-18 11:52:53 -0800

    WIP results function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,44 @@
 // UI Logic
-function results() {
-    makingAr();
-    let resultAr = [];
-    array.foreach(function (element) {
-        if (array.includes(1)) {
-            resultAr.push("Bleep!");
-        } else if (array.includes(2)) {
-            resultAr.push("Bloop!");
-        } else if (array.includes(3)) {
-            resultAr.push("Won't you be my neighbor?");
-        } else {
-            resultAr.push(array);
-        }
-
-    });
-    let p = document.getElementById("results");
-    p.innerText = array.join(", ");
-};
+// function results() {
+//     let resultAr = [];
+//     let array = [];
+//     makingAr();
+//     array.forEach(function (element) {
+//         if (element.includes(1)) {
+//             resultAr.push("Bleep!");
+//         } else {
+//             resultAr.push(element);
+//         };
+//         console.log(resultAr);
+//         let p = document.getElementById("results");
+//         p.innerText = resultAr.join(", ");
+//     });
+// };
 
 // Business Logic
-
 function makingAr() {
-    const inputNum = document.getElementById("number").value;
     let array = [];
+    const inputNum = document.getElementById("number").value;
 
     for (let index = 0; index <= inputNum; index += 1) {
         array.push(index);
     };
+};
+
+function results() {
+    let resultAr = [];
+    let array = [];
+    makingAr();
+    array.forEach(function (element) {
+        if (element.includes(1)) {
+            resultAr.push("Bleep!");
+        } else {
+            resultAr.push(element);
+        };
+        console.log(resultAr);
+        let p = document.getElementById("results");
+        p.innerText = resultAr.join(", ");
+    });
 };
 
 window.onload = function () {
